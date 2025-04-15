cd70844580cf65fb25259261bd97a503b2cecb8b
hossam <hossa@uop.edu.jo>
2025-04-15 13:49:01 +0300

    partials

--- shop/app.js
+++ shop/app.js
@@ -1,9 +1,11 @@
 const productRouter= require('./routes/product');
 const errorRouter = require('./routes/errors');
+const bodyparser = require('body-parser');
 const express = require("express");
 const app = express();
 app.set("view engine","ejs");
 app.set("views","views");
+app.use(bodyparser({urlencoded:false}));
 app.use(productRouter);
 app.use(errorRouter);
 
--- shop/controllers/product.js
+++ shop/controllers/product.js
@@ -5,7 +5,12 @@ const getIndex= (req,res)=>{
     res.render("home");
    };
 const postProduct= (req,res)=>{
-    res.send("Data Received");
+    res.render('products/viewproduct',
+        {   title:"View Product", 
+            n:req.body.name,
+            p: req.body.price,
+            t: req.body.type
+        });
    };
 exports.getProduct=getProduct;
 exports.postProduct=postProduct;
